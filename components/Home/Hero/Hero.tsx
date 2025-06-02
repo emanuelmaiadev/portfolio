d14b1a3adc7da613d0286f36a90f1b2b55882818
Emanuel Maia <emanuelmaiadev@gmail.com>
2025-06-02 15:00:53 -0300

    fix resume

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
             </p>
 
             <a
-              href="/resume.pdf"
+              href="/resume"
               download
               data-aos="zoom-in"
               data-aos-delay="300"
